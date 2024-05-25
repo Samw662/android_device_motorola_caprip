74e3bf605d061e5678f5898068a8bc1cc3c3e96a
Samw662 <amadorsamir307@gmail.com>
2024-05-25 02:39:21 +0200

    caprip: set my custom unofficial build id

--- lineage_caprip.mk
+++ lineage_caprip.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/motorola/caprip/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Build ID
+TARGET_UNOFFICIAL_BUILD_ID := Samm
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_caprip
 PRODUCT_DEVICE := caprip
